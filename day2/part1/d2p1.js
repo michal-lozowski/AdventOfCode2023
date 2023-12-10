1a11f62f61f99d5f4c5139c6d18102e3c001a0b0
Michal Lozowski <michal.lozowski@proton.me>
2023-12-10 18:02:32 +0100

    day 2 part 2

--- day2/part1/d2p1.js
+++ day2/part1/d2p1.js
@@ -18,9 +18,9 @@ fs.readFile("realinput.txt", 'utf8', (err, data) => {
         const patternBlue = /((1[5-9])|(2[0-9])) blue/;
 
         if ((!(patternRed.test(inputArray[i]))) && (!(patternGreen.test(inputArray[i]))) && (!(patternBlue.test(inputArray[i])))) {
-            console.log("A good game found! Game ID: " + (i+1))
+            //console.log("A good game found! Game ID: " + (i+1))
             counter = counter + (i+1);
-            console.log("Current counter state: " + counter)
+            //console.log("Current counter state: " + counter)
         }
     };
     console.log("Final Sum: " + counter);
---
+++ day2/part2/d2p2.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+fs.readFile("realinput.txt", 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading the file:', err);
+        return;
+    }
+
+    const inputText = data;
+    let inputArray = inputText.split("\r\n");
+    
+    let finalSum = 0;
+    for (let i = 0; i < inputArray.length; i++) {
+        
+        const patternRed = /(([0-9])|(1[0-9])|(2[0-9])) red/g;
+        const patternGreen = /(([0-9])|(1[0-9])|(2[0-9])) green/g;
+        const patternBlue = /(([0-9])|(1[0-9])|(2[0-9])) blue/g;
+
+        let maxRedFound = 1;
+        let maxGreenFound = 1;
+        let maxBlueFound = 1;
+
+        while ((match = patternRed.exec(inputArray[i])) !== null) {
+            matchedNumber = match[1];
+            if (parseInt(matchedNumber) > maxRedFound) maxRedFound = matchedNumber
+        }
+        
+        while ((match = patternGreen.exec(inputArray[i])) !== null) {
+            matchedNumber = match[1];
+            if (parseInt(matchedNumber) > maxGreenFound) maxGreenFound = matchedNumber
+        }
+        
+        while ((match = patternBlue.exec(inputArray[i])) !== null) {
+            matchedNumber = match [1];
+            if (parseInt(matchedNumber) > maxBlueFound) maxBlueFound = matchedNumber
+        }
+
+        //console.log (maxRedFound, maxGreenFound, maxBlueFound)
+        finalSum = (finalSum + (maxRedFound*maxGreenFound*maxBlueFound)) 
+
+    };
+    console.log("Final Sum: " + finalSum);
+});
